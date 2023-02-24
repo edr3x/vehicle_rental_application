d502a660f86ef446dce46e6eaf8cd07a114f1697
edr3x <dhunganaannanta@gmail.com>
2023-02-25 00:30:32 +0545

    init(verify): auth

---
+++ lib/views/auth/verify_phone.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VerifyPhone extends StatelessWidget {
+  static const String routeName = '/verify_phone';
+  const VerifyPhone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/views/exports.dart
+++ lib/views/exports.dart
@@ -1,2 +1,3 @@
 export 'home/home_page.dart';
-export 'login/login_page.dart';
+export 'auth/login_page.dart';
+export 'auth/verify_phone.dart';
