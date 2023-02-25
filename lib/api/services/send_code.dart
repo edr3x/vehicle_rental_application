ba6120d143cd1ff6198f865dadafff229872e9fc
edr3x <dhunganaannanta@gmail.com>
2023-02-25 12:13:07 +0545

    typo

--- lib/api/services/send_code.dart
+++ lib/api/services/send_code.dart
@@ -4,7 +4,7 @@ import 'package:rental_system_app/api/api.dart';
 import "../../utils/http_error_handler.dart";
 
 class SendCodeService {
-  Futire<dynamic> data({required int phoneNumber}) async {
+  Future<dynamic> data({required int phoneNumber}) async {
     http.Client client = http.Client();
 
     final Uri url = Uri.parse("$api/sendcode");
