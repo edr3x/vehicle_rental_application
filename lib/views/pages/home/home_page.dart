9889074256dcc7f5ab2dd222d2414f6ee1e08773
edr3x <dhunganaannanta@gmail.com>
2023-05-02 16:58:13 +0545

    refactor: custom home bar

--- lib/views/pages/home/home_page.dart
+++ lib/views/pages/home/home_page.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rental_system_app/api/blocs/user/get_user_details/get_user_details_cubit.dart';
-import 'package:rental_system_app/views/common/widgets/display_image.dart';
-import 'package:rental_system_app/views/pages/notification/notificatoni_page.dart';
-import 'package:rental_system_app/views/pages/profile/profile_page.dart';
+import 'package:rental_system_app/views/pages/home/widgets/custom_home_bar.dart';
 import 'package:rental_system_app/views/pages/search/search_result_page.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   static const String routeName = '/home-page';
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   String greeting() {
     DateTime now = DateTime.now();
 
@@ -29,8 +20,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var profile = context.read<GetUserDetailsCubit>().state.data.data!;
-
     void onSubmit(String value) {
       Navigator.pushNamed(
         context,
@@ -42,70 +31,28 @@ class _HomePageState extends State<HomePage> {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          title: RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: greeting(),
-                  style: const TextStyle(fontSize: 15),
-                ),
-                const TextSpan(text: "\n"),
-                TextSpan(
-                    text: profile.gender == "male"
-                        ? "Mr. ${profile.fullName}"
-                        : "Ms. ${profile.fullName}",
-                    style: const TextStyle(fontSize: 18)),
-              ],
-            ),
-          ),
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, ProfilePage.routeName);
-              },
-              icon: Hero(
-                tag: 'profile-hero',
-                child: CircleAvatar(
-                  backgroundImage: displayImage(profile.profileImage),
-                ),
-              ),
-            ),
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, NotificationPage.routeName);
-                },
-                icon: const Icon(Icons.notifications_outlined),
-              ),
-            ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const CustomHomeBar(),
+                  TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
                       ),
+                      labelText: "Find any vehicle ...",
+                      prefixIcon: Icon(Icons.search),
                     ),
-                    labelText: "Find any vehicle ...",
-                    prefixIcon: Icon(Icons.search),
+                    onSubmitted: onSubmit,
                   ),
-                  onSubmitted: onSubmit,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/views/pages/home/widgets/custom_home_bar.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rental_system_app/api/blocs/user/get_user_details/get_user_details_cubit.dart';
+import 'package:rental_system_app/constants/global_variables.dart';
+import 'package:rental_system_app/views/common/widgets/display_image.dart';
+import 'package:rental_system_app/views/pages/notification/notification_page.dart';
+import 'package:rental_system_app/views/pages/profile/profile_page.dart';
+
+class CustomHomeBar extends StatelessWidget {
+  String greeting() {
+    DateTime now = DateTime.now();
+
+    if (now.hour >= 0 && now.hour < 12) return "Good Morning";
+
+    if (now.hour >= 12 && now.hour < 17) return "Good Afternoon";
+
+    if (now.hour >= 17 && now.hour < 21) return "Good Evening";
+
+    return "Good Night";
+  }
+
+  const CustomHomeBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var profile = context.read<GetUserDetailsCubit>().state.data.data!;
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0, bottom: 25.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: GlobalVariables.cardBackgroundColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: IconButton(
+                  iconSize: 30,
+                  onPressed: () {
+                    Navigator.pushNamed(context, ProfilePage.routeName);
+                  },
+                  icon: Hero(
+                    tag: 'profile-hero',
+                    child: CircleAvatar(
+                      backgroundImage: displayImage(profile.profileImage),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: greeting(),
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    const TextSpan(text: "\n"),
+                    TextSpan(
+                      text: profile.gender == "male"
+                          ? "Mr. ${profile.fullName}"
+                          : "Ms. ${profile.fullName}",
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: GlobalVariables.cardBackgroundColor,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: IconButton(
+              iconSize: 26,
+              onPressed: () {
+                Navigator.pushNamed(context, NotificationPage.routeName);
+              },
+              icon: const Icon(Icons.notifications_outlined),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
