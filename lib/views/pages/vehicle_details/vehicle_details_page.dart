3c52febfad9dbb9eef1890690010dbca20e87db4
edr3x <dhunganaannanta@gmail.com>
2023-05-05 16:20:34 +0545

    refactor: seperate bottom bar

--- lib/views/pages/vehicle_details/vehicle_details_page.dart
+++ lib/views/pages/vehicle_details/vehicle_details_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rental_system_app/api/blocs/user/update_address_cubit/update_address_cubit.dart';
 import 'package:rental_system_app/api/blocs/vehicle/get_vehicle_details_cubit/get_vehicle_details_cubit.dart';
-import 'package:rental_system_app/constants/global_variables.dart';
 import 'package:rental_system_app/views/common/widgets/custom_error_dialogue.dart';
+import 'package:rental_system_app/views/pages/vehicle_details/widgets/bottom_bar.dart';
 
 class VehicleDetailsPage extends StatelessWidget {
   static const String routeName = '/vehicle-details-page';
@@ -39,61 +39,9 @@ class VehicleDetailsPage extends StatelessWidget {
               ],
             ),
           ),
-          bottomSheet: Container(
-            padding: const EdgeInsets.all(10),
-            height: 80,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: GlobalVariables.backgroundColor,
-              // color: Colors.white,
-            ),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "Rs.${details.rate!.split('/')[0]}",
-                          style: const TextStyle(
-                            fontSize: 25,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        TextSpan(
-                          text: "/${details.rate!.split('/')[1]}",
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                const Spacer(),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: GlobalVariables.primaryColor,
-                    minimumSize: const Size(220, 50),
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    "Book Now",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+          bottomSheet: DetailsPageBottomBar(
+            details: details,
+            buttonOnTap: () {},
           ),
         );
       },
---
+++ lib/views/pages/vehicle_details/widgets/bottom_bar.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:rental_system_app/api/models/vehicle/vehicle_details_model.dart';
+
+import '../../../../constants/global_variables.dart';
+
+class DetailsPageBottomBar extends StatelessWidget {
+  final Result details;
+  final VoidCallback? buttonOnTap;
+
+  const DetailsPageBottomBar({
+    super.key,
+    required this.details,
+    required this.buttonOnTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 80,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: GlobalVariables.backgroundColor,
+        // color: Colors.white,
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Rs.${details.rate!.split('/')[0]}",
+                    style: const TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "/${details.rate!.split('/')[1]}",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const Spacer(),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: GlobalVariables.primaryColor,
+              minimumSize: const Size(220, 50),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+            onPressed: buttonOnTap,
+            child: const Text(
+              "Book Now",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
