154386abc3dd650e28fd5c81dd697ff31be2614f
edr3x <dhunganaannanta@gmail.com>
2023-05-20 00:09:50 +0545

    `refactor:` small vehicle upload page logic

--- lib/views/pages/vehicle/add_vehicle_page.dart
+++ lib/views/pages/vehicle/add_vehicle_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:rental_system_app/api/blocs/vehicle/add_vehicle_cubit/add_vehicle_cubit.dart';
+import 'package:rental_system_app/views/common/widgets/custom_error_dialogue.dart';
+import 'package:rental_system_app/views/common/widgets/custom_snackbar.dart';
+import 'package:rental_system_app/views/pages/auth/widgets/auth_button.dart';
+import 'package:rental_system_app/views/pages/home/home_page.dart';
 
 class AddVehiclePage extends StatefulWidget {
   static const String routeName = '/add-vehicle';
@@ -10,9 +16,11 @@ class AddVehiclePage extends StatefulWidget {
 }
 
 class _AddVehiclePageState extends State<AddVehiclePage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  XFile? _image;
   final ImagePicker _picker = ImagePicker();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+
+  XFile? _image;
 
   void choosePhoto(ImageSource source) async {
     final pickedImage = await _picker.pickImage(source: source);
@@ -21,62 +29,67 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
     });
   }
 
+  void _submit() {
+    setState(() {
+      _autovalidateMode = AutovalidateMode.always;
+    });
+
+    final form = _formKey.currentState;
+    if (form == null || !form.validate()) return;
+
+    form.save();
+
+    //TODO: call bloc method here
+    //put this in image field:" _image!.path"
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (_) => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Form(
-              key: _formKey,
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  // TextFormField(
-                  //   keyboardType: TextInputType.number,
-                  //   autocorrect: false,
-                  //   maxLength: 10,
-                  //   decoration: const InputDecoration(
-                  //     border: OutlineInputBorder(
-                  //       borderRadius: BorderRadius.all(
-                  //         Radius.circular(10.0),
-                  //       ),
-                  //     ),
-                  //     labelText: 'Enter phone number',
-                  //     prefixIcon: Icon(Icons.phone),
-                  //   ),
-                  //   validator: (String? value) {
-                  //     if (value == null || value.isEmpty) {
-                  //       return 'Please enter your phone number';
-                  //     }
-                  //     if (value.length != 10) {
-                  //       return 'Please enter a valid phone number';
-                  //     }
-                  //     return null;
-                  //   },
-                  //   onSaved: (String? value) {},
-                  // ),
-                  // CustomAuthButton(text: "Next", onTap: submit),
-                  ElevatedButton(
-                    onPressed: () {
-                      choosePhoto(ImageSource.gallery);
-                    },
-                    child: const Text("Select Image"),
+    return BlocConsumer<AddVehicleCubit, AddVehicleState>(
+      listener: (context, state) {
+        if (state.status == AddVehicleStatus.error) {
+          errorDialog(context, state.error.errMsg);
+        }
+        if (state.status == AddVehicleStatus.loaded) {
+          customSnackBar(context, "Vehicle Added Successfully");
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            HomePage.routeName,
+            (route) => false,
+          );
+        }
+      },
+      builder: (context, state) {
+        return GestureDetector(
+          onTapDown: (_) => FocusScope.of(context).unfocus(),
+          child: Scaffold(
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Form(
+                  key: _formKey,
+                  autovalidateMode: _autovalidateMode,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          choosePhoto(ImageSource.gallery);
+                        },
+                        child: const Text("Select Image"),
+                      ),
+                      CustomAuthButton(
+                        text: state.status == AddVehicleStatus.loading ? "Posting....." : "Post",
+                        onTap: _submit,
+                      ),
+                    ],
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // await uploadImage(imageFile: _image!.path);
-                    },
-                    child: const Text("Upload Image"),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
