19fe4cfbc27a6cd425c756ec5474f9bd7fba9275
edr3x <dhunganaannanta@gmail.com>
2023-05-27 16:44:28 +0545

    `refactor:` show search results

--- lib/views/pages/search/search_result_page.dart
+++ lib/views/pages/search/search_result_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rental_system_app/api/blocs/vehicle/get_vehicle_details_cubit/get_vehicle_details_cubit.dart';
 import 'package:rental_system_app/api/blocs/vehicle/search_vehicle_cubit/search_vehicle_cubit.dart';
+import 'package:rental_system_app/constants/global_variables.dart';
+import 'package:rental_system_app/views/common/widgets/display_image.dart';
+import 'package:rental_system_app/views/pages/vehicle_details/vehicle_details_page.dart';
 
 class SearchResultPage extends StatelessWidget {
   static const String routeName = '/search-result-page';
@@ -13,14 +17,107 @@ class SearchResultPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Search Results'),
       ),
-      //TODO: improve UI
       body: ListView.builder(
+        scrollDirection: Axis.vertical,
         itemCount: result.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(result[index].title!),
-            subtitle: Text(result[index].id!),
-            trailing: Text(result[index].rate!),
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                VehicleDetailsPage.routeName,
+                arguments: true,
+              );
+              context.read<GetVehicleDetailsCubit>().getVehicleDetails(
+                    vehicleId: result[index].id!,
+                  );
+            },
+            child: Center(
+              child: Container(
+                margin: const EdgeInsets.only(top: 9, bottom: 9),
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: 220,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(30),
+                  color: GlobalVariables.cardBackgroundColor,
+                ),
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: displayVehicle(result[index].thumbnail, 20),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: 120,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20),
+                          ),
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              Colors.black.withOpacity(0.8),
+                              Colors.transparent,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 2),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.5),
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(18),
+                            bottomRight: Radius.circular(18),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              RichText(
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  text: result[index].title,
+                                  style: const TextStyle(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                result[index].rate ?? "rate",
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  color: Color.fromARGB(255, 233, 233, 233),
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         },
       ),
